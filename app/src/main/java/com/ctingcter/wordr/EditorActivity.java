87f27db6aeffa262ffc234d8ba35a3b5965839b4
Gareth Miller <gmiller290488@gmail.com>
2017-04-24 11:43:45 +0100

    Add add word function

--- app/src/main/java/com/ctingcter/wordr/EditorActivity.java
+++ app/src/main/java/com/ctingcter/wordr/EditorActivity.java
@@ -1,8 +1,67 @@
 package com.ctingcter.wordr;
 
-/**
- * Created by CTingCTer on 21/04/2017.
- */
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class EditorActivity {
+import com.ctingcter.wordr.data.WordContract;
+import com.ctingcter.wordr.data.WordDBHelper;
+
+public class EditorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mWordEditText;
+    private EditText mDefineEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_word);
+
+        mWordEditText = (EditText) findViewById(R.id.add_word_editText);
+        mDefineEditText = (EditText) findViewById(R.id.add_def_editText);
+        Button saveWord = (Button) findViewById(R.id.save_word);
+        saveWord.setOnClickListener(this);
+
+    }
+
+    private void insertWord() {
+        // Read from input fields
+        // Use trim to eliminate leading or trailing white space
+        String wordString = mWordEditText.getText().toString().trim();
+        String defString = mDefineEditText.getText().toString().trim();
+
+        // Create database helper
+        WordDBHelper mDBHelper = new WordDBHelper(this);
+
+        // Gets database in write mode
+        SQLiteDatabase db = mDBHelper.getWritableDatabase();
+        // Create a ContentValues object where column names are the keys
+        // And the Word "bulwark" and definition will be the values.
+        ContentValues values = new ContentValues();
+        values.put(WordContract.WordEntry.COLUMN_NAME_WORD, "Bulwark");
+        values.put(WordContract.WordEntry.COLUMN_DEFINITION, "a solid wall-like structure raised for defense");
+        // Insert a new row and return ID of that row
+        long newRowId = db.insert(WordContract.WordEntry.TABLE_NAME, null, values);
+
+        // show a toast message
+        if (newRowId == -1) {
+            // if row ID is -1 there was an error
+            Toast.makeText(this, "Error with inserting word", Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise it was successfully inserted
+            Toast.makeText(this, "Word saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        insertWord();
+    }
 }
+
+
--- app/src/main/java/com/ctingcter/wordr/MainActivity.java
+++ app/src/main/java/com/ctingcter/wordr/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ctingcter.wordr;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -7,8 +8,10 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.ctingcter.wordr.data.WordContract;
@@ -20,11 +23,25 @@ import com.ctingcter.wordr.data.WordDBHelper;
  */
 public class MainActivity extends AppCompatActivity {
 
+    // Database helper that will provide access to the database
+    private WordDBHelper mDBHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayDatabaseInfo();
+        mDBHelper = new WordDBHelper(this);
+
+        // Set up add word button to launch EditorActivity
+        Button addWord = (Button) findViewById(R.id.add_word);
+        addWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                        startActivity(intent);
+            }
+        });
     }
 
     /**
@@ -46,12 +63,49 @@ public class MainActivity extends AppCompatActivity {
             // Display the number of rows in the Cursor (which reflects the number of rows in the
             // pets table in the database).
             TextView displayView = (TextView) findViewById(R.id.word);
-            displayView.setText("Number of rows in pets database table: " + cursor.getCount());
+            displayView.setText("Number of rows in words database table: " + cursor.getCount());
         } finally {
             // Always close the cursor when you're done reading from it. This releases all its
             // resources and makes it invalid.
             cursor.close();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on "insert random word" menu option
+            case R.id.action_insert_random_word:
+                insertWord();
+                displayDatabaseInfo();
+                return true;
+            // Respond to a click on the "Delete all words" menu option
+            case R.id.action_delete_all_entries:
+                // Fill out this part later
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void insertWord() {
+        // gets the databse in write mode
+        SQLiteDatabase db = mDBHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys
+        // And the Word "bulwark" and definition will be the values.
+        ContentValues values = new ContentValues();
+        values.put(WordEntry.COLUMN_NAME_WORD, "Bulwark");
+        values.put(WordEntry.COLUMN_DEFINITION, "a solid wall-like structure raised for defense");
+
+        long newRowId = db.insert(WordEntry.TABLE_NAME, null, values);
     }
 }
 
